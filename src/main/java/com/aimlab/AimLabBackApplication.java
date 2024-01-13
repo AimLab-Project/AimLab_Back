47755431168f1a60d2d63c7819327f7f2bbec1ae
YeonsangYoon <1004dustkd@gmail.com>
2024-01-13 10:12:41 +0900

    first commit

---
+++ src/main/java/com/aimlab/AimLabBackApplication.java
@@ -0,0 +1,13 @@
+package com.aimlab;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AimLabBackApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AimLabBackApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/aimlab/AimLabBackApplicationTests.java
@@ -0,0 +1,13 @@
+package com.aimlab;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AimLabBackApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
